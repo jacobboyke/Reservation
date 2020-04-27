e2084ea1e006bad50679b7ca13ab60c2251aca2c
jboyke <jacob.boyke14@gmail.com>
2020-04-27 14:58:44 -0500

    2nd Commit. Added IdentitySamples

--- Reservations.UI/Startup.cs
+++ Reservations.UI/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Reservations.UI.Startup))]
 namespace Reservations.UI
 {
     public partial class Startup
