25a1e5b4585afe84515d43c24a6a0c14ee2dd6f3
jboyke <jacob.boyke14@gmail.com>
2020-04-27 11:35:55 -0500

    First Commit. Added UI and Data layers

---
+++ Reservations.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Reservations.UI.MvcApplication" Language="C#" %>
---
+++ Reservations.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Reservations.UI.Startup))]
+namespace Reservations.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
